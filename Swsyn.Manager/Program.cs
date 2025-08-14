1a071f228a6b5ad2fedab0b1d96d4e77b16a94c9
carrotypirate <arrotypirate@mail.ru>
2025-08-14 21:30:07 +0500

    Добавьте файлы проекта.

---
+++ Swsyn.Manager/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Swsyn.Manager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
