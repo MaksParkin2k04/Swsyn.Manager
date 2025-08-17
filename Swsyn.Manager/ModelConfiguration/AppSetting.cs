7beb6915fe8c86b7183f524fa36a4ac7faeadafa
carrotypirate <arrotypirate@mail.ru>
2025-08-17 13:08:05 +0500

    добавление модели appsetting

---
+++ Swsyn.Manager/ModelConfiguration/AppSetting.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swsyn.Manager.ModelConfiguration
+{
+    public class AppSetting
+    {
+        public string SourcePath { get; set; } = string.Empty;
+
+        public Dictionary<string, ProjectOption> Projects { get; set; }
+
+        public string[]? Include { get; set; }
+    }
+}
