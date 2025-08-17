ad9c9b549d371c57a29e2399e63972c27a610744
carrotypirate <arrotypirate@mail.ru>
2025-08-17 13:52:37 +0500

    добавление настроек appsettings.json

--- Swsyn.Manager/ModelConfiguration/AppSettings.cs
+++ Swsyn.Manager/ModelConfiguration/AppSettings.cs
@@ -6,12 +6,10 @@ using System.Threading.Tasks;
 
 namespace Swsyn.Manager.ModelConfiguration
 {
-    public class AppSetting
+    public class AppSettings
     {
         public string SourcePath { get; set; } = string.Empty;
-
         public Dictionary<string, ProjectOption> Projects { get; set; }
-
         public string[]? Include { get; set; }
     }
 }
--- Swsyn.Manager/Program.cs
+++ Swsyn.Manager/Program.cs
@@ -1,14 +1,51 @@
-﻿namespace Swsyn.Manager
+﻿using Swsyn.Manager.ModelConfiguration;
+using Microsoft.Extensions.Configuration;
+
+namespace Swsyn.Manager
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Launch();
+            try
+            {
+                IConfigurationRoot config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+                AppSettings? settings = config.Get<AppSettings>();
 
+                if (settings == null) { throw new Exception("Settings Error"); }
+               // View(settings);
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.ToString());
+            }
+
+           
+
+            Launch();
         }
 
+        //static void View(AppSettings settings)
+        //{
+        //    Console.WriteLine($"SourcePath: '{settings.SourcePath}'.");
+        //    Console.WriteLine();
 
+        //    Console.WriteLine("Projects:");
+        //    Console.WriteLine();
+
+        //    foreach (string projectName in settings.Projects.Keys)
+        //    {
+        //        ProjectOption projectOption = settings.Projects[projectName];
+        //        Console.WriteLine($"Project Name: '{projectName}'.");
+        //        Console.WriteLine($"Target Path: '{projectOption.TargetPath}'.");
+        //        Console.WriteLine($"Contractor: '{projectOption.Contractor}'.");
+        //        Console.WriteLine($"Customer: '{projectOption.Customer}'.");
+        //        Console.WriteLine();
+        //    }
+
+        //    Console.WriteLine($"Include: {string.Join(", ", settings.Include.Select(i => $"'{i}'"))}");
+        //}
 
         static void Launch()
         {
